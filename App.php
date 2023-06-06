bf1713ef4b97a10cfc3adca889125c63706346ea
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-06-06 13:11:23 +0700

    update App.php include Repository

--- App.php
+++ App.php
@@ -1,6 +1,8 @@
 <?php
   require_once __DIR__ . "/Entity/Todolist.php";
+  require_once __DIR__ . "/Repository/TodolistRepository.php";
 
   use Entity\Todolist;
+  use Repository\TodolistRepositoryImpl;
 
   echo "Aplikasi Todolist" . PHP_EOL;
