abb7e982e31b96281278027445a7e34593fe455b
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-06-06 13:07:00 +0700

    update TodolistRepository.php menampilkan todolist

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -9,4 +9,24 @@
       function remove(int $number): bool;
       function findAll(): array;
     }
+
+    class TodolistRepositoryImpl implements TodolistRepository
+    {
+      public array $todolist = array();
+
+      function save(Todolist $todolist): void
+      {
+
+      }
+
+      function remove(int $number): bool
+      {
+
+      }
+
+      function findAll(): array
+      {
+        return $this->todolist;
+      }
+    }
   }
