5a15b30f8ad6a7cc01f484ad682390671738d074
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-06-06 14:56:58 +0700

    update App.php include Service

--- App.php
+++ App.php
@@ -1,8 +1,10 @@
 <?php
   require_once __DIR__ . "/Entity/Todolist.php";
   require_once __DIR__ . "/Repository/TodolistRepository.php";
+  require_once __DIR__ . "/Service/TodolistService.php";
 
   use Entity\Todolist;
   use Repository\TodolistRepositoryImpl;
+  use Service\TodolistServiceImpl;
 
   echo "Aplikasi Todolist" . PHP_EOL;
