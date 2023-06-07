d4e91b3227b777549ac7f7fe110ef418a2d781cd
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-06-07 14:08:14 +0700

    update App.php include Helper

--- App.php
+++ App.php
@@ -2,9 +2,11 @@
   require_once __DIR__ . "/Entity/Todolist.php";
   require_once __DIR__ . "/Repository/TodolistRepository.php";
   require_once __DIR__ . "/Service/TodolistService.php";
+  require_once __DIR__ . "/Helper/InputHelper.php";
 
   use Entity\Todolist;
   use Repository\TodolistRepositoryImpl;
   use Service\TodolistServiceImpl;
+  use Helper\InputHelper;
 
   echo "Aplikasi Todolist" . PHP_EOL;
